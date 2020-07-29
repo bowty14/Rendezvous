831fc44f0bccdef15d872d2b2036c1a0d89c8a52
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-07-29 10:45:48 -0700

    add landing page, create logo for landing page.

--- src/components/RestaurantRoutes.js
+++ src/components/RestaurantRoutes.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Switch, Route } from 'react-router-dom';
 import RandomRestaurant from './RandomRestaurant';
 import RestaurantList from './RestaurantList';
+import Home from './Home';
+
 
 const RestaurantRoutes = () => {
   return (
     <Switch>
       <Route exact path='/random' component={RandomRestaurant}></Route>
       <Route path='/restaurant' component={RestaurantList}></Route>
+      <Route path='/' component={Home}></Route>
     </Switch>
   );
 }
